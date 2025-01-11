8e83c761dca6156b382e9c006c82543bea2eca7b
Alexito-Hub <alessandrovillogas@outlook.es>
2025-01-10 19:28:36 -0500

    Agregar clase Tiktok con métodos para descargar, buscar y obtener tendencias de videos

--- index.js
+++ index.js
@@ -125,6 +125,10 @@ const start = async() => {
         const args = body.slice(cmd ? prefix.find(prefix => body.toLowerCase().startsWith(prefix.toLowerCase())).length + command.length : command.length).trim().split(/ +/)
 
 		const metadata = from.endsWith("@g.us") ? await client.groupMetadata(from) : false
+		const admins = metadata ? metadata.participants.filter(i => i.admin == "admin" || i.admin == "superadmin").map(i => i.id) : false
+		const isAdmin = metadata ? admins.includes(sender) : false
+		const isBotAdmin = metadata ? admins.includes(client.user.jid) : false
+
 		const expiration = msg?.extendedTextMessage?.contextInfo?.expiration ?? msg?.contextInfo?.expiration ?? null
 		
 		const Number = from.endsWith("@g.us") ? v.key.participant : from
@@ -193,6 +197,105 @@ const start = async() => {
 				break
 			}
 
+			case "kick":
+			case "add": {
+				if (!isBotAdmin) return client.sendMessage(from, { text: "*No se puede usar esta función si no soy administrador.*" })
+				if (!isAdmin) return client.sendMessage(from, { text: "*Esta función es solo para los administradores.*" })
+
+				if (command === "add") {
+					if (!args.join(" ")) return client.sendMessage(from, { text: "*Escriba el número a agregar.*" })
+
+					let user = mentionedJid[0] ? mentionedJid : quoted ? [quoted.sender] : args.join(" ").replace(/[^0-9]/g, '') + '@s.whatsapp.net'
+					const adding = await client.groupParticipantsUpdate(from, [user], "add")
+
+					if (adding[0].status === "403") {
+						let link = 'https://chat.whatsapp.com/' + (await client.groupInviteCode(from))
+						await client.sendMessage(user, { 
+							text: `*Hola! Fuiste invitado para unirte a este grupo.*`, 
+							contextInfo: { 
+								externalAdReply: { 
+									title: metadata.subject, 
+									mediaType: 1, 
+									thumbnailUrl: "https://telegra.ph/file/5fa373768ae095c367026.jpg", 
+									mediaUrl: link, 
+									sourceUrl: link 
+								} 
+							} 
+						})
+						await client.sendMessage(from, { text: "*Invitación enviada.*" })
+					} else if (adding[0].status === "408") {
+						await client.sendMessage(from, { text: "*No se pudo agregar porque abandonó el grupo recientemente. Inténtalo de nuevo más tarde.*" })
+					} else if (adding[0].status === "401") {
+						await client.sendMessage(from, { text: "*El usuario me ha bloqueado, no lo puedo agregar.*" })
+					} else if (adding[0].status === "200") {
+						await client.sendMessage(from, { text: "*El usuario fue agregado con éxito.*" })
+					} else if (adding[0].status === "409") {
+						await client.sendMessage(from, { text: "*El usuario ya se encuentra en el grupo.*" })
+					}
+				} else if (command === "kick") {
+					let user = quoted ? quoted.sender : mentionedJid ? mentionedJid[0] : args.join(" ").replace(/[^0-9]/g, '') + "@s.whatsapp.net"
+					if (!user) return client.sendMessage(from, { text: "*Marque un mensaje o use @ para elegir a quien eliminar.*" })
+					if (client.user.jid === user) return client.sendMessage(from, { text: "*No puedo autoeliminarme.*" })
+					if (admins.includes(user) && !isOwner) return client.sendMessage(from, { text: "*Mis permisos no me permiten eliminar a otro administrador.*" })
+					if (user === sender) return client.sendMessage(from, { text: "*No puedes autoeliminarte.*" })
+					if (mods.includes(user.split("@")[0])) return client.sendMessage(from, { text: "*Lo siento, este usuario es un moderador y no puede ser eliminado por el bot.*" })
+
+					await client.groupParticipantsUpdate(from, [user], "remove")
+					await client.sendMessage(from, { text: `*El usuario @${user.split("@")[0]} ya no forma parte del grupo.*`, mentions: [user] })
+				}
+				break
+			}
+
+			case "promote":
+			case "demote": {
+				if (!isBotAdmin) return client.sendMessage(from, { text: "*No se puede usar esta función si no soy administrador.*" });
+				if (!isAdmin) return client.sendMessage(from, { text: "*Esta función es solo para los administradores.*" })
+
+				let user = quoted ? quoted.sender : (mentionedJid.length != 0) ? mentionedJid[0] : body.replace(/[0-9]/i, "") + "@s.whatsapp.net";
+				if (!user) return client.sendMessage(from, { text: "*Marque un mensaje o use @ para elegir a quien darle o quitar administración.*" });
+
+				if (command === "promote") {
+					if (admins.includes(user)) return client.sendMessage(from, { text: "*Este usuario ya posee privilegios de administrador.*" });
+					await client.groupParticipantsUpdate(from, [user], "promote");
+					await client.sendMessage(from, { 
+						text: `*El usuario @${user.split("@")[0]} ha recibido el cargo de administrador por un _super usuario_.*`, 
+						mentions: [...await client.getAdmins(from), user].map(i => i) 
+					});
+				} else if (command === "demote") {
+					if (!admins.includes(user)) return client.sendMessage(from, { text: "*Este usuario no posee privilegios de administrador.*" });
+					await client.groupParticipantsUpdate(from, [user], "demote");
+					await client.sendMessage(from, { 
+						text: `*El usuario @${user.split("@")[0]} se eliminó del cargo de administrador por un _super usuario_.*`, 
+						mentions: [...await client.getAdmins(from), user].map(i => i) 
+					});
+				}
+				break
+			}
+
+            case "join":
+			case "unirse": {
+				if (!isOwner) return client.sendMessage(from, { text: "*Lo siento, esto es una función exclusiva para moderadores y el dev.*" })
+				if (!args.join(" ")) return client.sendMessage(from, { text: "*Ingrese un link de invitación para poder unirme al grupo.*" })
+
+				let code = args.join(" ").split("chat.whatsapp.com/")[1]
+				let data = await client.groupGetInviteInfo(code)
+
+				if (Object.keys(store.groupMetadata).includes(data.id)) {
+					await client.sendMessage(from, { text: "*Ya me encuentro en ese grupo*" })
+					if (from != data.id) {
+						return await client.sendMessage(data.id, { text: `*Aquí estoy, jefe. ¿En qué puedo ayudarle?* @${Number}`, mentions: [Number] })
+					} else return
+				}
+
+				let joined = await client.groupAcceptInvite(code)
+				if (joined) {
+					await client.sendMessage(from, { text: `*Me uní correctamente a ${data.subject}*` })
+					await client.sendMessage(data.id, { text: `*Hola a todos, soy ${bot.name}. Fui invitado por @${sender} para administrar este grupo. Espero llevarnos bien.*`, mentions: [sender] })
+				} else {
+					await client.sendMessage(from, { text: "*Recuerde que puedo estar en la lista de espera o si no tiene activada esa opción, intente agregarme manualmente.*" })
+				}
+				break
+			}
             case "leave":
 			case "salir":{
 				if (!isOwner) return client.sendMessage(from, { text: "*Lo siento esto es una funcion exclusiva para moderadores.*"});
@@ -200,12 +303,11 @@ const start = async() => {
 				await delay(5000)
 				await client.groupLeave(from).then(async() => {
                     await client.sendMessage(sender, { text: "*Ya sali del grupo " + metadata.subject + "*"})
-				}).catch(async(err) => {
-                    if (err) return
+				}).catch(async() => {
 					await client.sendMessage(from, { text: "*Lo siento hay algo que me impide salir. Intentelo manualmente.*"})
 				})
+                break
 			}
-			break;
 			default: {
 			    if (body.startsWith('$')) {
 					if(!isOwner) return
